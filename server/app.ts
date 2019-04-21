ec95ed8b02b4c955c3459f93c8bc22f6b2ed01bb
Brock Taban <hello@brocktaban.com>
2019-04-21 13:55:38 -0400

    Added Twitch Server

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,61 @@
 import * as tmi from 'tmi.js'
+import { firestore } from 'firebase-admin'
 import { Firebase } from './firebase'
 
 import * as env from '../env.json'
 
+const database = new Firebase()
 export class Twitch {
 
     constructor() { this.init() }
 
     private async init() {
-        
+        const channels = await database.getChannes()
+
+        const opts = {
+            options: {
+                debug: true
+            },
+            identity: env,
+            channels: channels
+        };
+
+        const client = tmi.client(opts)
+
+        client.on('message', async (target, context, msg, self) => {
+            if (self) return
+            if (msg.charAt(0) != '?') return
+
+            const channelName = target.replace('#', '')
+            const username = context.username
+
+            const channel = await database.getChannel(channelName)
+            const channelData = channel.data()
+
+            if (!channel.exists || channelData == undefined)
+                return client.say(target, `@${username}, ${target} is not playing Valerian Games`)
+                
+            const commands = await database.getGameCommands(channelData.playing)
+
+            const cooldown: number = channelData.loopInMills // 600000
+            const timestamp: firestore.Timestamp = channelData.lastCommand
+            const currentTime = new Date()
+
+            const diffence = currentTime.getTime() - timestamp.toMillis()
+
+            console.log(diffence, cooldown, cooldown > diffence)
+
+            if (cooldown > diffence) 
+                return
+
+            commands.forEach(async c => {
+                if (msg.trim() == c.trim()) {
+                    client.say(target, `${c} it is!`);
+                    database.live(channelName, c, username)
+                }
+            });
+        });
+
+        client.connect()
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCqayEm17YA-fk9m8vqNGi00CqnQVcndZQ",
+    authDomain: "valerian-games-dev.firebaseapp.com",
+    databaseURL: "https://valerian-games-dev.firebaseio.com",
+    projectId: "valerian-games-dev",
+    storageBucket: "valerian-games-dev.appspot.com",
+    messagingSenderId: "1049630574760"
+  }
 };
