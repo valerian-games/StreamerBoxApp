f1e98180b30e3f0f1ac0c8f8a6ad06c65a31f9e5
Brock Taban <hello@brocktaban.com>
2019-04-20 21:11:32 -0400

    Designed Home Page
    Created Home Module
    Created Auth Service
    Added Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCqayEm17YA-fk9m8vqNGi00CqnQVcndZQ",
+    authDomain: "valerian-games-dev.firebaseapp.com",
+    databaseURL: "https://valerian-games-dev.firebaseio.com",
+    projectId: "valerian-games-dev",
+    storageBucket: "valerian-games-dev.appspot.com",
+    messagingSenderId: "1049630574760"
+  }
 };
 
 /*
