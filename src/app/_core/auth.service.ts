fb83a316078e518699e2f3c0ff3dffd03a35978d
Brock Taban <hello@brocktaban.com>
2019-04-21 21:25:21 -0400

    Changed colours on Redirect Component Changed to location.href

--- src/app/_core/auth.service.ts
+++ src/app/_core/auth.service.ts
@@ -1,27 +1,26 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private router: Router) { }
 
   signInWithTwitch() {
-    // window.location.href = "http://stackoverflow.com";
-    const popup = window.open('https://us-central1-valerian-games-dev.cloudfunctions.net/oAuthRedirect', '_blank', 'height=700,width=800')
+    window.location.href = "https://us-central1-valerian-games-dev.cloudfunctions.net/oAuthRedirect";
+  
   }
-  customSignIn(token: string) {
-    console.log('test')
-    return this.afAuth.auth.signInWithCustomToken(token).then(() => window.close() )
+  async customSignIn(token: string) {
+    await this.afAuth.auth.signInWithCustomToken(token).then(() => {
+      window.close()
+      
+    } )
+    this.router.navigate(['/dashboard']);
   }
-
-  closeWindow() {
-    window.close()
-  }
-
 
   getIdToken() {
     return this.afAuth.auth.currentUser.getIdToken()
