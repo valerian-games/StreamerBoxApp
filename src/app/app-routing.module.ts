f9fcc36c259255b30bea5b1ec6b94ccf0e814b5a
Brock Taban <hello@brocktaban.com>
2019-04-21 21:08:30 -0400

    Designed Redirect Module Created Dashboard Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { RedirectComponent } from './_routes/redirect/redirect.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'c/redirect', component: RedirectComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RedirectComponent } from './_routes/redirect/redirect.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    RedirectComponent
+    RedirectComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
