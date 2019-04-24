a0e75a4940fbecd9d200482966a15ec1808b199e
Brock Taban <hello@brocktaban.com>
2019-04-23 23:25:08 -0400

    Designed Command Create

--- src/app/dashboard/commands/commands.component.ts
+++ src/app/dashboard/commands/commands.component.ts
@@ -12,6 +12,9 @@ export class CommandsComponent implements OnInit {
   commands: {}[]
   yourCommands: {}[]
   showYourCommands = false
+  showAddCommand = false
+  games: {}[]
+  gameTags: {}[] = []
 
   constructor(
     private firestore: FirestoreService,
@@ -21,9 +24,25 @@ export class CommandsComponent implements OnInit {
   async ngOnInit() {
     this.commands = await this.firestore.commands
     this.yourCommands = await this.firestore.commandsBy(await this.auth.uid)
+    this.games = await this.firestore.games
     
     if (this.yourCommands.length > 0)
       this.showYourCommands = true
   }
 
+  removeTag(tag) {
+    this.gameTags = this.gameTags.filter(item => item !== tag)
+  }
+
+  addTag(tagId) {
+    this.gameTags.push(this.search(tagId, this.games))
+  }
+
+  private search(id, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].id === id) {
+            return myArray[i];
+        }
+    }
+}
 }
